efc065b365182fdf3806c0e9d27270eeb199e925
Nikolay Amirov <rflameheart@gmail.com>
2019-07-22 01:38:35 +0300

    Rename fix pt2 (done)

---
+++ Assets/scripts/GunController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    private int i = 0;
+    [SerializeField]
+    GameObject[] projectiles;
+    GameObject[] canonBall;
+    ProjectileController canon;
+    [SerializeField]
+    private int speed;
+
+    void Start()
+    {
+        foreach (GameObject projectile in projectiles)
+        {
+            canon = projectile.GetComponent<ProjectileController>();
+            canon.speed = speed;
+        }
+    }
+    public void Fire()
+    {   if (i >= projectiles.Length)
+        {
+            i = 0;
+        }
+
+        if (projectiles[i].activeSelf == false)
+        {
+            projectiles[i].transform.position = transform.position;
+            projectiles[i].transform.rotation = transform.rotation;
+            projectiles[i].SetActive(true);
+        }
+        i++;
+        }
+
+
+    private void OnDestroy()
+    {
+        if (projectiles[i] != null) canon.DestroyProjectile();
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Tank
+{
+    private float h, v;
+    Rigidbody2D rb2d;
+    GunController gun;
+    void Start()
+    {
+        gun = GetComponentInChildren<GunController>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (h != 0 && !isMoving) StartCoroutine(MoveHorizontal(h, rb2d));
+        else if (v != 0 && !isMoving) StartCoroutine(MoveVertical(v, rb2d));
+    }
+
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            gun.Fire();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb2d.velocity = Vector2.zero;
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage();
+        }
+    }
+}
